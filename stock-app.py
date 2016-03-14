78d17edb339662f3c0e503e30f47583937edda5b
Jay Qi <jay.y.qi@gmail.com>
2016-03-14 01:27:03 -0500

    Working embedded Bokeh plot.

--- stock-app.py
+++ stock-app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template, request, redirect
+import requests
+import pandas as pd
+
+from bokeh.plotting import figure
+from bokeh.embed import components
 
 app = Flask(__name__)
 
@@ -6,21 +11,34 @@ app.vars={}
 
 @app.route('/')
 def main():
-  return redirect('/userform')
+    return redirect('/userform')
 
-@app.route('/userform',methods=['GET','POST'])
+@app.route('/userform',methods=['GET'])
 def userstockform():
-    if request.method == 'GET':
-        return render_template('userstockform.html')
-    else:
-        print 'boo'
-        app.vars['tickersymbol'] = request.form['tickersymbol']
-        print 'hoo'
-        print request.form
-        print app.vars
+    return render_template('userstockform.html')
 
-        return render_template('results.html',tickersymbol=app.vars['tickersymbol'])
+@app.route('/results',methods=['POST'])
+def stockresults():
+
+    # Read user input from form
+    tickersymbol = request.form['tickersymbol']
+
+    # Get data and create dataframe
+    dataurl = 'https://www.quandl.com/api/v3/datasets/WIKI/' + tickersymbol + '.json'
+    r = requests.get(dataurl)
+    dataset_df = pd.DataFrame(r.json())['dataset'].apply(pd.Series)
+    data = dataset_df.ix['data',:].apply(pd.Series)
+    data.columns = dataset_df.ix['column_names',0:12]
+    data['Date'] = pd.to_datetime(data['Date'])
+
+    p = figure(title=tickersymbol)
+    p.line(data['Date'],data['Open'])
+
+    script, div = components(p)
+
+    return render_template('results.html',tickersymbol=tickersymbol, script=script, div=div)
+
 
 
 if __name__ == '__main__':
-  app.run(port=33507,debug=True)
+    app.run(port=33507,debug=True)
