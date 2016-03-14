a5075b786587ee43a8d39216ce6c10d3c6d764a0
Jay Qi <jay.y.qi@gmail.com>
2016-03-14 16:04:04 -0500

    Working version of app.

--- stock-app.py
+++ stock-app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request, redirect
 import requests
 import pandas as pd
+import datetime
+import dateutil
+import re
 
 from bokeh.plotting import figure
 from bokeh.embed import components
 
 app = Flask(__name__)
-
-app.vars={}
 
 @app.route('/')
 def main():
@@ -30,15 +31,32 @@ def stockresults():
     data = dataset_df.ix['data',:].apply(pd.Series)
     data.columns = dataset_df.ix['column_names',0:12]
     data['Date'] = pd.to_datetime(data['Date'])
+    today = datetime.date.today()
+    lastmonth = today - dateutil.relativedelta.relativedelta(months=1)
+    data_lastmonth = data[data['Date']>lastmonth]
 
-    p = figure(title=tickersymbol)
-    p.line(data['Date'],data['Open'])
+    name = re.search("(^.*\))",dataset_df.ix['name',0]).group(1)
+
+    p = figure(title=name,plot_width=600,plot_height=400,
+        x_axis_type="datetime",
+        x_range=(lastmonth,today),
+        x_axis_label = "Date",
+        y_axis_label = "USD")
+    if 'openingprice' in request.form:
+        p.line(data_lastmonth['Date'],data_lastmonth['Open'],line_color='blue',legend='Opening price')
+    if 'adjopeningprice' in request.form:
+        p.line(data_lastmonth['Date'],data_lastmonth['Adj. Open'],line_color='blue',line_dash=[4,4],legend='Adj. opening price')
+    if 'closingprice' in request.form:
+        p.line(data_lastmonth['Date'],data_lastmonth['Close'],line_color='red',legend='Closing price')
+    if 'adjclosingprice' in request.form:
+        p.line(data_lastmonth['Date'],data_lastmonth['Adj. Close'],line_color='red',line_dash=[4,4],legend='Adj. closing price')
+    p.legend.orientation = 'top_left'
 
     script, div = components(p)
 
-    return render_template('results.html',tickersymbol=tickersymbol, script=script, div=div)
+    return render_template('results.html',name=name, tickersymbol=tickersymbol, script=script, div=div)
 
 
 
 if __name__ == '__main__':
-    app.run(port=33507,debug=True)
+    app.run(port=33507)
